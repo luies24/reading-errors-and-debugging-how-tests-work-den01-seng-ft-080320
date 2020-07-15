44cf195b685c2f74bb921c48f7ebd884f0216e66
Luis Garcia <l.garcia24@hotmail.com>
2020-07-15 04:50:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,8 +4,3 @@ second_number = 22
 
 sum = first_number + second_number
 
-differnce = first_number - second_number
-
-#product = first_number * second_number
-
-#quotient = first_number / second_number
