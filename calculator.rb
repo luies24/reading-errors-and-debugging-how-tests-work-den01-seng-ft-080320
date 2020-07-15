3cdf62eac4196a8eb8e61c975d4ff9ee0d5e9eb2
Luis Garcia <l.garcia24@hotmail.com>
2020-07-15 04:49:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-# Add your variables here
+first_number = 44
+
+second_number = 22
+
+sum = first_number + second_number
+
+#differnce = first_number - second_number
+
+#product = first_number * second_number
+
+#quotient = first_number / second_number
